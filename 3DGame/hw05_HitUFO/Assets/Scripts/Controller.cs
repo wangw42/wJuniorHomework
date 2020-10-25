d4719d87023ad2d900d79abcfd28e67b3c3d3344
wangw42 <1070094087@qq.com>
2020-10-25 18:34:00 +0800

    hw05 init

---
+++ 3DGame/hw05_HitUFO/Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : System.Object
+{
+    private static Controller _instance;
+
+    public Model currentModel {get; set;}
+    public bool running{get; set;}
+
+    public static Controller getInstance(){
+        if(_instance == null){
+            _instance = new Controller();
+        }
+        return _instance;
+    }
+
+    public int getFPS(){
+        return Application.targetFrameRate;
+    }
+
+    public void setFPS(int fps){
+        Application.targetFrameRate = fps;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ 3DGame/hw05_HitUFO/Assets/Scripts/View.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IUserAction
+{
+    void GameOver();
+}
+
+public class View : MonoBehaviour
+{
+    private Model action;
+    private GUIStyle fontstyle0 = new GUIStyle();
+    private GUIStyle fontstyle = new GUIStyle();
+    private GUIStyle fontstyle1 = new GUIStyle();
+
+    void Start()
+    {
+        action = Controller.getInstance().currentModel as Model;
+        fontstyle0.fontSize = 48;
+        fontstyle0.normal.textColor = Color.yellow;
+        fontstyle.fontSize = 32;
+        fontstyle.normal.textColor = Color.black;
+        fontstyle1.fontSize = 32;
+        fontstyle1.normal.textColor = Color.yellow;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnGUI(){
+        GUI.Label(new Rect(10,10,200,200),"HitUFO: Yellow 1, Blue 2, Purple 3", fontstyle0);
+        GUI.Label(new Rect(10,60,200,200),"Round: " + action.Round, fontstyle);
+        GUI.Label(new Rect(10,90,200,200),"Level: " + action.Level, fontstyle);
+        GUI.Label(new Rect(10,120,200,200),"Score: " + action.Score, fontstyle);
+       
+        if (GUI.Button(new Rect(10, 180, 100, 50), "Restart")){
+            action.Restart();
+        }        
+    }
+
+}
